0d6c6d2fabdcca5e8e02d308c7b114635d493b1a
jitterxx <sergey@reshim.com>
2015-08-05 21:28:29 +0300

    тест

--- prototype 1/modules/untitled0.py
+++ prototype 1/modules/untitled0.py
@@ -6,7 +6,7 @@ Created on Fri Jul 24 17:04:18 2015
 """
 
 import datetime
-import prototype1_objects_and_orm_mappings as rw
+import prototype1_objects_and_orm_mappings as rwObjects
 import json
 import sys
 reload(sys)
@@ -15,4 +15,14 @@ sys.setdefaultencoding("utf-8")
 
 l = [['name','Имя'],['surname','Фамилия'],['login','Имя пользователя'],['password','Пароль']]
 
-print l[][0]
+
+session = rwObjects.Session()
+
+t = rwObjects.get_by_uuid('c34519e0-39e1-11e5-9c9e-f46d04d35cbd')[0]
+
+print t.VIEW_FIELDS[:]
+print t.__dict__['surname']
+
+print t.get_attrs()
+
+
