6a379cd7ad34ff39c8feed5f95d70f31d5265a47
root <root@rework.reshim.com>
2015-08-24 10:03:22 +0300

    added

---
+++ prototype 1/www/new_schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `rework_test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE USER 'rework_test'@'localhost' IDENTIFIED BY 'Cthutq123';
+GRANT ALL PRIVILEGES ON rework_test.* TO 'rework_teste'@'localhost' IDENTIFIED BY 'Cthutq123' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+
+
+
+
+
+
