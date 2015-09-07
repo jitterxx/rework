dd797eed6b7d9ab4ef30166394c30b4fa2b48438
jitterxx <sergey@reshim.com>
2015-09-07 13:35:18 +0300

    Обновленные конфиги

--- prototype 1/modules/configurations.py
+++ prototype 1/modules/configurations.py
@@ -6,10 +6,10 @@ Created on Wed Jul 22 16:23:47 2015
 """
 
 LEARN_PATH = "learn_machine/"
-mongo_uri = 'mongodb://flw_mongo_admin:Qazcde123@localhost/flw'
-sql_uri = 'mysql://rework:HtdjhR123@localhost/rework?charset=utf8'
-default_classifier = '4b5d3fd6-5546-11e5-a199-f46d04d35cbd'
-default_neighbors_classifier = '4b71f070-5546-11e5-a199-f46d04d35cbd'
-QUEUE_NAME = 'local_test'
+mongo_uri = 'mongodb://... : ...@localhost/ ...'
+sql_uri = 'mysql://... : ...@localhost/ ... ?charset=utf8'
+default_classifier = ''
+default_neighbors_classifier = ''
+QUEUE_NAME = ''
 QUEUE_BROKER = 'amqp://guest@localhost//'
 QUEUE_BROKER_BACKEND = 'rpc://'
